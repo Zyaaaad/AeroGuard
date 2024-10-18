a2fd0ab4f5185a2e8ba3bfb8d5e445c82d9ec20a
Zyaaaad <zyad.khalef@ensta-bretagne.org>
2024-10-18 08:19:15 +0200

    dernier vendredi matin

--- mes_signatures.h
+++ mes_signatures.h
@@ -1,10 +1,1 @@
 #include "mes_structures.h"
-
-unsigned char* load_image(const char* chemin, int* largeur, int* hauteur, int* comp);
-Drone creer_drone(int id, float x, float y, float z, float V, Status status);
-//void initDrone(Drone *, int, float, float, float, float, Status);
-void ajouter_drone(int id, float x, float y,float z,float V, Status status);
-void deplacerDrone(Drone *, float, float, float);
-void afficherPosition(Drone *);
-void afficherStatut(Drone *);
-void changerStatus(Drone *, Status);
