2a5eb42c7e1ff6bdf90c1fa4979e5e1246ba8339
Zyaaaad <zyad.khalef@ensta-bretagne.org>
2024-10-19 13:10:37 +0200

    comment

--- Essais/init_2.c
+++ Essais/init_2.c
@@ -1,7 +1,7 @@
-#define STB_IMAGE_WRITE_IMPLEMENTATION
+/* #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "STB/stb_image_write.h"
 #define STB_IMAGE_IMPLEMENTATION
-#include "STB/stb_image.h"
+#include "STB/stb_image.h" */
 
 #include <math.h>
 #include <stdio.h>
@@ -11,7 +11,8 @@
 #define alpha 0.05
 #define MAX_NEIGHBORS 4
 
-typedef struct {
+typedef struct
+{
     int id;
     float x, y, z; // position
     float coverage;
@@ -26,18 +27,21 @@ typedef struct {
 Drone drones[MAX_DRONES];
 int drone_count = 0;
 
-void spread_drones_simple(int num_drones, float xmin, float ymin, float xmax, float ymax) {
-    float surface=(xmax - xmin)*(ymax - ymin);
-    float cov=surface/num_drones;
-    float h=sqrt(cov)/(2*tan(alpha));
-    float grid_width =sqrt(cov);
-    float grid_height =sqrt(cov);
-    
+void spread_drones_simple(int num_drones, float xmin, float ymin, float xmax, float ymax)
+{
+    float surface = (xmax - xmin) * (ymax - ymin);
+    float cov = surface / num_drones;
+    float h = sqrt(cov) / (2 * tan(alpha));
+    float grid_width = sqrt(cov);
+    float grid_height = sqrt(cov);
+
     int drone_id = 0;
-    printf("Surface = %f\n",xmax*ymax);
-    printf("Coverage = %f\n",cov);
-     for (float x = xmin; x < xmax && drone_id < num_drones; x += grid_width) {
-        for (float y = ymin; y < ymax && drone_id < num_drones; y += grid_height) {
+    printf("Surface = %f\n", xmax * ymax);
+    printf("Coverage = %f\n", cov);
+    for (float x = xmin; x < xmax && drone_id < num_drones; x += grid_width)
+    {
+        for (float y = ymin; y < ymax && drone_id < num_drones; y += grid_height)
+        {
             drones[drone_id].id = drone_id + 1;
             drones[drone_id].x = x + grid_width / 2;
             drones[drone_id].y = y + grid_height / 2;
@@ -49,29 +53,30 @@ void spread_drones_simple(int num_drones, float xmin, float ymin, float xmax, fl
     drone_count = drone_id;
 }
 
-int main() {
-    int num_drones,drone_id;
+int main()
+{
+    int num_drones, drone_id;
     printf("give me num of drones: ");
-    scanf("%d",&num_drones);
+    scanf("%d", &num_drones);
     int width, height;
     printf("give me width: ");
-    scanf("%d",&width);
+    scanf("%d", &width);
     printf("give me height: ");
-    scanf("%d",&height);
-    spread_drones_simple(num_drones, 0, 0, width, height);    
+    scanf("%d", &height);
+    spread_drones_simple(num_drones, 0, 0, width, height);
 
-    drone_id=0;
-    
-    for (int i = 0; i < num_drones; i++) {
-           printf("%d\n",drones[drone_id].id);
-           printf("%f\n",drones[drone_id].x);
-           printf("%f\n",drones[drone_id].y);
-           printf("%f\n",drones[drone_id].z);
-           printf("%f\n",drones[drone_id].coverage);
+    drone_id = 0;
 
-            drone_id++;
-        }
+    for (int i = 0; i < num_drones; i++)
+    {
+        printf("%d\n", drones[drone_id].id);
+        printf("%f\n", drones[drone_id].x);
+        printf("%f\n", drones[drone_id].y);
+        printf("%f\n", drones[drone_id].z);
+        printf("%f\n", drones[drone_id].coverage);
+
+        drone_id++;
+    }
 
     return 0;
-
 }
